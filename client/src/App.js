1e3a19135af18792bb1b211fd7c749f96e14aca3
HaxagonusD <jquezad1@binghamton.edu>
2021-12-16 09:44:17 -0500

    Made the game multiplayer (multi-client)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { useState, useEffect, useRef } from "react";
 import generate4DigitNumber from "./utils/generate4DigitNumber";
+import { io } from "socket.io-client";
 
 function App() {
+  //game state
   const [randomNumbers, setRandomNumbers] = useState([]);
   const [numberOfTries, setNumberOfTries] = useState(10);
   const [gameOver, setGameOver] = useState(false);
@@ -9,17 +11,74 @@ function App() {
   const [hints, setHints] = useState("");
   const [winningTracker, setWinningTracker] = useState([0, 0, 0, 0]);
 
+  //state related to socket io
+  const [referenceToSocket, setReferenceToSocket] = useState();
+  const [enemyclientId, setEnemyClientid] = useState(undefined);
+  const [myClientId, setMyClientid] = useState(undefined);
+
+  const [enemyClientGameState, setEnemyClientGameState] = useState({
+    winningTracker: [0, 0, 0, 0],
+    wonGame: false,
+    gameOver: false,
+    numberOfTries: 10,
+  });
+
+  //take this client states and turn it into a format the enemy client can read
+  const createSnapshotOfMyState = () => {
+    return {
+      winningTracker,
+      wonGame,
+      gameOver,
+      numberOfTries,
+    };
+  };
+
+  //references to input elements
   const inputOne = useRef(undefined);
   const inputTwo = useRef(undefined);
   const inputThree = useRef(undefined);
   const inputFour = useRef(undefined);
+  //join game input element
+  const joinGameInput = useRef(undefined);
 
+  //socketio
+  const SERVER = "http://localhost:8080";
+  useEffect(() => {
+    const socket = io(SERVER, {
+      cors: {
+        origin: "*",
+      },
+    });
+    socket.on("receive:personal_id", (data) => {
+      setMyClientid(data);
+    });
+    socket.on("being_joined", (data) => {
+      console.log("being_joined", data);
+      setEnemyClientid(data.myClientId);
+    });
+    //implement these
+    socket.on("i_lost", () => {
+      setWonGame(true);
+    });
+    socket.on("i_won", () => {
+      setGameOver(true);
+    });
+    socket.on("receive:updated_enemy_state", (data) => {
+      setEnemyClientGameState(data);
+    });
+
+    setReferenceToSocket(socket);
+    return () => socket.disconnect();
+  }, []);
+
+  //set the initials numbers to guess when the component first loads
   useEffect(() => {
     (async () => {
       setRandomNumbers(await generate4DigitNumber());
     })();
   }, []);
 
+  //determine which message to display as a hint when the user makes a guess
   const messageMultiplexer = (numberInput, location) => {
     const correctNumber = randomNumbers[location];
     let message = "";
@@ -40,6 +99,7 @@ function App() {
     setHints(message);
   };
 
+  //handlers for every button. could be done using #id's of the button elements
   const handleButtonClickOne = () => {
     setNumberOfTries(numberOfTries - 1);
     messageMultiplexer(inputOne.current.value, 0);
@@ -57,44 +117,66 @@ function App() {
     messageMultiplexer(inputFour.current.value, 3);
   };
 
-  // resets all the state effectively restarting the game
-  const handleRestart = async () => {
-    setNumberOfTries(10);
-    setRandomNumbers(await generate4DigitNumber());
-    setGameOver(false);
-    setWonGame(false);
-    setWinningTracker([0, 0, 0, 0]);
-  };
-
+  //check if the user is out of tried
   useEffect(() => {
     if (numberOfTries === 0) {
       setGameOver(true);
     }
   }, [numberOfTries]);
 
+  // check is the user won the game
   useEffect(() => {
     setWonGame(winningTracker.every((currentElement) => currentElement === 1));
   }, [winningTracker]);
 
   const determineDisplayBasedOnGameState = () => {
+    if (!enemyclientId) {
+      return (
+        <div>
+          <h1>Waiting for another client to connect</h1>
+          <h2>Send this id: {myClientId}</h2>
+          <h3>Or you can join another client by typing in their id</h3>
+          <input ref={joinGameInput}></input>
+          <button
+            onClick={() => {
+              setEnemyClientid(joinGameInput.current.value);
+              referenceToSocket.emit("joining_you", {
+                myClientId,
+                enemyclientId: joinGameInput.current.value,
+              });
+            }}
+          >
+            Join Game
+          </button>
+        </div>
+      );
+    }
+
     if (wonGame) {
+      referenceToSocket.emit("i_won", enemyclientId);
       return (
         <div>
           <h1>Congrats! You've won!</h1>
-          <button onClick={handleRestart}>Restart</button>
+          <h2>The numbers were: {randomNumbers}</h2>
+          <p>Refresh the page to start again</p>
         </div>
       );
     } else if (gameOver) {
+      referenceToSocket.emit("i_lost", enemyclientId);
       return (
         <div>
           <h1>GameOver</h1>
-          <button onClick={handleRestart}>Restart</button>
+          <h2>The numbers were: {randomNumbers}</h2>
+          <p>Refresh the page to start again</p>
         </div>
       );
     } else {
       return (
         <div>
-          <h1>Type in numbers 0-7 to guess the right code!</h1>
+          <h1>
+            Type in numbers 0-7 to guess the right code! Find it faster than
+            your opponent!
+          </h1>
           <h2>You have {numberOfTries} guesses, Choose wisely</h2>
           <p>{hints}</p>
           <input ref={inputOne} type="number"></input>
@@ -105,6 +187,7 @@ function App() {
           <button onClick={handleButtonClickThree}>Guess!</button>
           <input ref={inputFour} type="number"></input>
           <button onClick={handleButtonClickFour}>Guess!</button>
+          <p>{`${2}`}</p>
         </div>
       );
     }
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+const port = 8080;
+
+io.on("connection", (socket) => {
+  socket.emit("receive:personal_id", socket.id);
+  socket.on("joining_you", (data) => {
+    console.log("joining_you", data);
+    socket.to(data.enemyclientId).emit("being_joined", data);
+  });
+
+  socket.on("i_won", (enemyClientId) => {
+    socket.to(enemyClientId).emit("i_won");
+  });
+  socket.on("i_lost", (enemyClientId) => {
+    socket.to(enemyClientId).emit("i_lost");
+  });
+});
+
+server.listen(port, () => {
+  console.log("listening on localhost:8080");
+});
